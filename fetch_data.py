64c73ad54f6f381b2a56621c3768504f64228b60
Kevin Desai <kevindesai777@gmail.com>
2016-04-03 02:03:10 -0400

    Code cleanup

--- fetch_data.py
+++ fetch_data.py
@@ -6,7 +6,6 @@ import json
 fname = "app.config"
 CON = json.load(file(fname, 'r'))
 
-
 class Fetch_news(object):
     def __init__(self, consumer_key=None, consumer_secret=None, access_token_key=None, access_token_secret=None):
         self.auth = OAuth1(consumer_key, consumer_secret, access_token_key, access_token_secret)
@@ -15,26 +14,22 @@ class Fetch_news(object):
         url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
         self.response = requests.get(url, params=params, auth=self.auth, timeout=10)
 
-
 def main():
       
     key = "xxxx"
     secret = "xxxx"
     token = "xxxx"
     token_secret ="xxxx"
-    
     news = Fetch_news(key, secret, token, token_secret)
-     
     id = {account["name"]: 0 for account in CON["accounts"]}
 
-  
     while True:
         try:
             for account in CON["accounts"]:
                 name = account["name"]
                 file_name = account["file"]
-
                 params = {'screen_name':name, 'exclude_replies':'true', 'count':'200'}
+                
                 if id[name] > 0:
                     params['max_id'] = id[name]
                 news.fetch_tweets(params)
@@ -54,7 +49,6 @@ def main():
 
         except Exception as e:
             print "Error"
-
 
 if __name__ == "__main__":
     main()
